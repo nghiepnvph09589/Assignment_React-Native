dcba9b8b7f2d8f205306de89b5dbb21ca7ac02fc
nghiepnvph09589 <nguyennghiep2007@gmail.com>
2020-10-22 08:12:28 +0700

    nghiep2

--- Screen/DetailPhoto.js
+++ Screen/DetailPhoto.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Image,TouchableOpacity, } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+import * as FileSystem from 'expo-file-system';
 
 export default function DetailPhoto({ navigation, route }) {
     const { urlpt } = route.params;
+
+    let download = async() =>{
+      const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if(status==="granted"){
+        const fileuri = FileSystem.documentDirectory+"aaa.jpg"
+        FileSystem.downloadAsync(urlpt, fileuri).then(({uri})=>{
+          MediaLibrary.createAssetAsync(fileuri);
+        })
+      }
+    }
+
+    
+
+    
+
     return (
         <View style={styles.container}>
             <Text>{urlpt}</Text>
             <Image style={{ height:300, width: '100%', }} source={{ uri: urlpt }} />
             <StatusBar style="auto" />
+            <TouchableOpacity style={{ margin: 10 }} onPress={()=>{download()}}>
+                <Feather name='save' size={50} color='#121212' />
+              </TouchableOpacity>
         </View>
     );
 }
